3043556d6dd60e5752f81b8fa021842d4df2f3ac
Joyce <joyceannie111@gmail.com>
2023-11-15 07:20:11 -0800

    UI changes

--- app/page.tsx
+++ app/page.tsx
@@ -41,7 +41,7 @@ export default function Page() {
       <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
 
         <h1 className="sm:text-6xl text-4xl max-w-[708px] font-bold text-slate-900">
-          Generate your AI generated LinkedIn post
+          Create your AI generated LinkedIn post
         </h1>
         <form className="max-w-xl w-full" onSubmit={onSubmit}>
           <div className="flex mt-10 items-center space-x-3">
