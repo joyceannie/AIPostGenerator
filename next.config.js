212f6223528e952fa971834b01ee0611f36e9fd2
Joyce <joyceannie111@gmail.com>
2023-11-15 02:04:41 -0800

    Updated next.config.js

--- next.config.js
+++ next.config.js
@@ -2,16 +2,6 @@
 module.exports = {
   async redirects() {
     return [
-      {
-        source: '/github',
-        destination: 'https://github.com/Nutlope/twitterbio',
-        permanent: false,
-      },
-      {
-        source: '/deploy',
-        destination: 'https://vercel.com/templates/next.js/twitter-bio',
-        permanent: false,
-      },
     ];
   },
 };
