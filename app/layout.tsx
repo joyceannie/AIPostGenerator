82da3cd123e30deec786d16049b8513d165ff604
Joyce <joyceannie111@gmail.com>
2023-11-15 00:16:12 -0800

    Post generator

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import { Analytics } from '@vercel/analytics/react';
+import '../styles/globals.css';
+
+export default function RootLayout({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <html lang="en">
+      <body>
+        {children}
+        <Analytics />
+      </body>
+    </html>
+  );
+}
---
+++ app/page.tsx
@@ -0,0 +1,124 @@
+'use client';
+
+import Image from 'next/image';
+import { useRef, useState } from 'react';
+import { Toaster, toast } from 'react-hot-toast';
+import Footer from '../components/Footer';
+import Header from '../components/Header';
+import { useChat } from 'ai/react';
+
+export default function Page() {
+  const [bio, setBio] = useState('');
+  const bioRef = useRef<null | HTMLDivElement>(null);
+
+  const scrollToBios = () => {
+    if (bioRef.current !== null) {
+      bioRef.current.scrollIntoView({ behavior: 'smooth' });
+    }
+  };
+
+  const { input, handleInputChange, handleSubmit, isLoading, messages } =
+    useChat({
+      body: {
+        bio,
+      },
+      onResponse() {
+        scrollToBios();
+      },
+    });
+
+  const onSubmit = (e: any) => {
+    setBio(input);
+    handleSubmit(e);
+  };
+
+  const lastMessage = messages[messages.length - 1];
+  const generatedBios = lastMessage?.role === "assistant" ? lastMessage.content : null;
+
+  return (
+    <div className="flex max-w-5xl mx-auto flex-col items-center justify-center py-2 min-h-screen">
+      <Header />
+      <main className="flex flex-1 w-full flex-col items-center justify-center text-center px-4 mt-12 sm:mt-20">
+
+        <h1 className="sm:text-6xl text-4xl max-w-[708px] font-bold text-slate-900">
+          Generate your next LinkedIn post using ChatGPT
+        </h1>
+        <form className="max-w-xl w-full" onSubmit={onSubmit}>
+          <div className="flex mt-10 items-center space-x-3">
+
+            <p className="text-left font-medium">
+              What kind of post are you looking for?{' '}
+              <span className="text-slate-500">
+                (or write a few sentences about yourself)
+              </span>
+            </p>
+          </div>
+          <textarea
+            value={input}
+            onChange={handleInputChange}
+            rows={4}
+            className="w-full rounded-md border-gray-300 shadow-sm focus:border-black focus:ring-black my-5"
+            placeholder={
+              'e.g. Entrepreneur looking for innovative ideas.'
+            }
+          />
+          {!isLoading && (
+            <button
+              className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
+              type="submit"
+            >
+              Generate LinkedIn post &rarr;
+            </button>
+          )}
+          {isLoading && (
+            <button
+              className="bg-black rounded-xl text-white font-medium px-4 py-2 sm:mt-10 mt-8 hover:bg-black/80 w-full"
+              disabled
+            >
+              <span className="loading">
+                <span style={{ backgroundColor: 'white' }} />
+                <span style={{ backgroundColor: 'white' }} />
+                <span style={{ backgroundColor: 'white' }} />
+              </span>
+            </button>
+          )}
+        </form>
+        <Toaster
+          position="top-center"
+          reverseOrder={false}
+          toastOptions={{ duration: 2000 }}
+        />
+        <hr className="h-px bg-gray-700 border-1 dark:bg-gray-700" />
+        <output className="space-y-10 my-10">
+          {generatedBios && (
+            <>
+              <div>
+                <h2
+                  className="sm:text-4xl text-3xl font-bold text-slate-900 mx-auto"
+                  ref={bioRef}
+                >
+                  Your generated post
+                </h2>
+              </div>
+              <div className="space-y-8 flex flex-col items-center justify-center max-w-xl mx-auto">
+                <div
+                  className="bg-white rounded-xl shadow-md p-4 hover:bg-gray-100 transition cursor-copy border"
+                  onClick={() => {
+                    navigator.clipboard.writeText(generatedBios);
+                    toast('Bio copied to clipboard', {
+                      icon: '✂️',
+                    });
+                  }}
+                  key={generatedBios}
+                >
+                  <p>{generatedBios}</p>
+                </div>
+              </div>
+            </>
+          )}
+        </output>
+      </main>
+      <Footer />
+    </div>
+  );
+}
